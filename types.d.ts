67f1550383951ed06c5f60f73a6d57c72345cd26
JasonMun7 <jasonmun020104@gmail.com>
2024-06-26 00:17:45 -0600

    Added in new types file in order to help with native wind configuration

---
+++ types.d.ts
@@ -0,0 +1,11 @@
+// types.d.ts
+import "nativewind/types";
+
+declare module "react-native" {
+  interface TextProps {
+    className?: string;
+  }
+  interface ViewProps {
+    className?: string;
+  }
+}
